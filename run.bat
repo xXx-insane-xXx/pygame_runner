21b0279422257eb660ad4b9c29caa752ab20e4da
insane <shreyanshsaha19@proton.me>
2024-08-26 09:35:32 +0530

    Seperate linux and windows venv

--- run.bat
+++ run.bat
@@ -1,15 +1,15 @@
 @echo off
 REM Check if the virtual environment exists
-if not exist venv (
+if not exist win_venv (
     echo Error: Virtual environment not found. Please ensure the venv directory is included.
     exit /b 1
 )
 
 REM Activate the virtual environment
-call venv\Scripts\activate
+call win_venv\Scripts\activate
 
 REM Run the game using pythonw to suppress the console
-venv\Scripts\pythonw.exe main.py
+win_venv\Scripts\pythonw.exe main.py
 
 REM Deactivate the virtual environment (optional, if you want to clean up)
 deactivate
--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Check if the virtual environment exists
-if [ ! -d "venv" ]; then
+if [ ! -d "linux_venv" ]; then
     echo "Error: Virtual environment not found. Please ensure the venv directory is included."
     exit 1
 fi
 
 # Activate the virtual environment
-source venv/bin/activate
+# source linux_venv/bin/activate
 
 # Run the game
-venv/bin/python3 main.py > /dev/null 2>&1 &
+linux_venv/bin/python3 main.py > /dev/null 2>&1 &
 
 # Deactivate the virtual environment (optional)
 deactivate
